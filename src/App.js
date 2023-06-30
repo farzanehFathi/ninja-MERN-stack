f41404c3d59e9c0f9e4d59f04354e24870b4610a
farzaneh <farzaneh.fth@gmail.com>
2023-06-30 18:07:57 +0430

    Navigation Bar added and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Weather from "./Weather";
 import Search from "./Search";
+import NavigationBar from "./NavigationBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavigationBar />
         <Search />
         <h1>Hello from React </h1>
 
