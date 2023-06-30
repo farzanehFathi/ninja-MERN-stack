635f219f641d73abe0051d21e74c0512aa37a218
farzaneh <farzaneh.fth@gmail.com>
2023-06-30 18:52:00 +0430

    Weather component completed

--- src/Weather.js
+++ src/Weather.js
@@ -2,12 +2,27 @@ import React from "react";
 import "./Weather.css";
 // import axios from "axios";
 
-export default function Weather({}) {
+export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Lisbon </h1>
-      <div className="weather-detail__text">Friday 17:30</div>
-      <div className="weather-detail__text">Clear</div>
+      <div className="weather-header">
+        <h1>Lisbon </h1>
+        <div className="weather-detail__text">Friday 17:30</div>
+        <div className="weather-detail__text">Clear</div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 temperature">
+          <div className="weather-icon">icon</div>
+          <div className="weather-temp">28</div>
+          <div className="weather-temp-unit">°C</div>
+        </div>
+
+        <div className="col-sm-6">
+          {" "}
+          <div className="weather-detail__text">Precipitation: 42%</div>
+          <div className="weather-detail__text">Wind: 12km/h</div>
+        </div>
+      </div>
     </div>
   );
 }
