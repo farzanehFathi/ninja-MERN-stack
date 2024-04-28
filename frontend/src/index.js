02ffebae5ce316fa41cd4c2ac87aab6fd2937dc0
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-28 20:10:05 +0330

    front-end is almost done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { workoutsContetxProvider } from "./context/WorkoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <workoutsContetxProvider>
+      <App />
+    </workoutsContetxProvider>
   </React.StrictMode>
 );
